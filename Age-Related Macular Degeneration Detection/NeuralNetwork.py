a5342067b333332b27416bd0cc514910ef0a4170
Bill Bai <31229051+billbai0102@users.noreply.github.com>
2020-08-04 16:29:15 -0400

    implemented transform functions

--- Age-Related Macular Degeneration Detection/NeuralNetwork.py
+++ Age-Related Macular Degeneration Detection/NeuralNetwork.py
@@ -3,10 +3,12 @@ from torch import nn
 from torchsummary import summary
 import yaml
 
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-    def forward(self, x):
+    @staticmethod
+    def forward(x):
         return x
 
