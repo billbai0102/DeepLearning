a4bc34f6efc69ede39b6d075926241d68fe29715
Bill Bai <31229051+billbai0102@users.noreply.github.com>
2020-09-01 22:27:43 -0400

    Lo and behold, I've finally finished!! (After hours of tuning hyperparameters cuz GANs are just like that)

---
+++ DCGAN MNIST/Discriminator.py
@@ -0,0 +1,44 @@
+import torch
+from torch import nn
+import torchsummary
+
+from hyperparameters import *
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.dis = nn.Sequential(
+            # Block 1
+            nn.Conv2d(CHANNELS, DIS_HIDDEN * 1, 4, 2, 1, bias=False)
+            , nn.LeakyReLU(.2, inplace=True),
+
+            # Block 2
+            nn.Conv2d(DIS_HIDDEN * 1, DIS_HIDDEN * 2, 4, 2, 1, bias=False)
+            , nn.BatchNorm2d(DIS_HIDDEN * 2)
+            , nn.LeakyReLU(.2, inplace=True),
+
+            # Block 3
+            nn.Conv2d(DIS_HIDDEN * 2, DIS_HIDDEN * 4, 4, 2, 1, bias=False)
+            , nn.BatchNorm2d(DIS_HIDDEN * 4)
+            , nn.LeakyReLU(.2, inplace=True),
+
+            # Block 4
+            nn.Conv2d(DIS_HIDDEN * 4, DIS_HIDDEN * 8, 4, 2, 1, bias=False)
+            , nn.BatchNorm2d(DIS_HIDDEN * 8)
+            , nn.LeakyReLU(.2, inplace=True),
+
+            # Output
+            nn.Conv2d(DIS_HIDDEN * 8, 1, 4, 1, 0, bias=False)
+            , nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.dis(x)
+        x = x.view(-1, 1)
+        x = x.squeeze(1)
+        return x
+
+
+if __name__ == '__main__':
+    print(torchsummary.summary(Discriminator().cuda(), input_size=(1, 100,100)))
--- DCGAN MNIST/Generator.py
+++ DCGAN MNIST/Generator.py
@@ -4,7 +4,6 @@ import torchsummary
 
 from hyperparameters import *
 
-
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
@@ -12,23 +11,32 @@ class Generator(nn.Module):
             # Block 1
             nn.ConvTranspose2d(GEN_IN, GEN_HIDDEN * 8, 4, 1, 0, bias=False)
             , nn.BatchNorm2d(GEN_HIDDEN * 8)
-            , nn.ReLU(inplace=True),
+            , nn.LeakyReLU(0.2, inplace=True),
 
             # Block 2
-            nn.ConvTranspose2d(GEN_IN, GEN_HIDDEN * 8, 4, 1, 0, bias=False)
-            , nn.BatchNorm2d(GEN_HIDDEN * 8)
-            , nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(GEN_HIDDEN * 8, GEN_HIDDEN * 4, 4, 2, 1, bias=False)
+            , nn.BatchNorm2d(GEN_HIDDEN * 4)
+            , nn.LeakyReLU(0.2, inplace=True),
 
             # Block 3
-            nn.ConvTranspose2d(GEN_IN, GEN_HIDDEN * 4, 4, 1, 0, bias=False)
-            , nn.BatchNorm2d(GEN_HIDDEN * 4)
-            , nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(GEN_HIDDEN * 4, GEN_HIDDEN * 2, 4, 2, 1, bias=False)
+            , nn.BatchNorm2d(GEN_HIDDEN * 2)
+            , nn.LeakyReLU(0.2, inplace=True),
 
             # Block 4
-            nn.ConvTranspose2d(GEN_IN, GEN_HIDDEN * 2, 4, 1, 0, bias=False)
-            , nn.BatchNorm2d(GEN_HIDDEN * 2)
-            , nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(GEN_HIDDEN * 2, GEN_HIDDEN * 1, 4, 2, 1, bias=False)
+            , nn.BatchNorm2d(GEN_HIDDEN * 1)
+            , nn.LeakyReLU(0.2, inplace=True),
+
+            # Output block
+            nn.ConvTranspose2d(GEN_HIDDEN * 1, CHANNELS, 4, 2, 1, bias=False)
+            , nn.Tanh()
         )
 
     def forward(self, x):
         x = self.gen(x)
+        return x
+
+
+if __name__ == '__main__':
+    print(torchsummary.summary(Generator().cuda(), input_size=(100, 1, 1)))
