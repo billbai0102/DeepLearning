e8683ebcc2f99f7b4617fc118a45ddc9799859eb
Bill Bai <31229051+billbai0102@users.noreply.github.com>
2020-09-03 01:16:19 -0400

    completed discriminator

--- CycleGAN MNIST/Generator.py
+++ CycleGAN MNIST/Generator.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch
 from torch import nn
-from torch import functional as F
 
 from hyperparameters import *
 
@@ -18,19 +17,19 @@ class Generator(nn.Module):
 
         self.embedding = nn.Embedding(self.classes, self.classes)
 
-        def layer(in_size, out_size, normalize=True):
+        def layer(in_sz, out_sz, normalize=True):
             """
             Returns an fc layer w/ BatchNorm and LeakyReLU
-            :param in_size: input size
-            :param out_size: output size
+            :param in_sz: input size
+            :param out_sz: output size
             :param normalize: bool whether to apply BatchNorm
             :return: layer
             """
             # add fc layer (bias is False if BatchNorm comes after)
-            layers = [nn.Linear(in_size, out_size, bias=(not normalize))]
+            layers = [nn.Linear(in_sz, out_sz, bias=(not normalize))]
             # add BatchNorm layer if normalize is True
             if normalize:
-                layers.append(nn.BatchNorm1d(out_size))
+                layers.append(nn.BatchNorm1d(out_sz))
             # add LeakyReLU activation
             layers.append(nn.LeakyReLU(.2, inplace=True))
 
@@ -56,5 +55,7 @@ class Generator(nn.Module):
         # noise
         z = torch.cat((self.embedding(labels), noise), -1)
         x = self.gen(z)
+
+        # reshape to image
         x = x.view(x.size(0), *self.img_shape)
         return x
