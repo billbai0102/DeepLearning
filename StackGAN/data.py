f9d28921977d1f5ef03352dae93ba239ce90cdec
Bill Bai <31229051+billbai0102@users.noreply.github.com>
2020-09-16 22:23:04 -0400

    data

--- StackGAN/data.py
+++ StackGAN/data.py
@@ -1,6 +1,9 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 
+import h5py
+import numpy as np
+
 class DataStream:
     def __init__(self, dl):
         self.dl = iter(dl)
@@ -28,3 +31,26 @@ class DataStream:
         self.load_next_data()
 
         return cur_input, cur_target
+
+
+class StackGANDataset(Dataset):
+    def __init__(self, file, transform, split='train'):
+        self.file = file
+        self.transform = transform
+        self.split = split
+
+        self.dataset = None
+        self.dataset_keys = None
+
+        self.h5_int = lambda x: int(np.array(x))
+
+    def __len__(self):
+        f = h5py.File(self.file, 'r')
+        self.dataset_keys = [str(key) for key in f[self.split].keys()]
+        length = len(f[self.split])
+        f.close()
+
+        return length
+
+    def __getitem__(self, idx):
+
